e61ce7c0a2828837181f50225bd87c3f7966573f
phantekzy <mainilotfi@gmail.com>
2026-02-25 19:11:58 +0100

    defining the tables

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,3 +1,9 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { text, timestamp } from "drizzle-orm/gel-core";
+import { pgTable, uuid } from "drizzle-orm/pg-core";
 
-export const users = pgTable();
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
