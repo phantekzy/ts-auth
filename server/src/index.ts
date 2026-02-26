6d1a44f7da67f41290153c9c6055895362dc839d
phantekzy <mainilotfi@gmail.com>
2026-02-26 17:45:58 +0100

    added json() and cookiesParser and cors logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
