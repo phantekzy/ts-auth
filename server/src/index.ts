eed5d00c9a7b3216d1c53060eb5dea4deb8f1c8a
phantekzy <mainilotfi@gmail.com>
2026-02-26 18:19:41 +0100

    index.ts configuration done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import authRoutes from "./routes/auth.js";
 const app = express();
 
 app.use(express.json());
@@ -13,3 +13,10 @@ app.use(
     credentials: true,
   }),
 );
+
+app.use("auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
