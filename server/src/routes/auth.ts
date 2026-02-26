972131f7bce50d313ea3ac97ceb7a82afc23eca4
phantekzy <mainilotfi@gmail.com>
2026-02-26 18:29:04 +0100

    insert new users to the database logic

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import argon2 from "argon2";
+import { db } from "../db/index.js";
+import { users } from "../db/schema.js";
 
 const router = Router();
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
+  try {
+    const hash = await argon2.hash(password);
+    const [newUser] = await db
+      .insert(users)
+      .values({
+        email,
+        passwordHash: hash,
+      })
+      .returning();
+  } catch (error) {}
 });
 
 export default router;
