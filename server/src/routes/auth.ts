1ae9bb614dd56a583c498966f22eea89bd63b351
phantekzy <mainilotfi@gmail.com>
2026-02-26 18:46:33 +0100

    added email validation check and password length rule

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,9 +4,18 @@ import { db } from "../db/index.js";
 import { users } from "../db/schema.js";
 
 const router = Router();
-
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
+  if (!email || password) {
+    return res
+      .status(400)
+      .json({ error: "Both email and password are required!" });
+  }
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 8 characters" });
+  }
   try {
     const hash = await argon2.hash(password);
     const [newUser] = await db
@@ -16,12 +25,12 @@ router.post("/signup", async (req, res) => {
         passwordHash: hash,
       })
       .returning();
-
     res.status(201).json({ message: "User created!", id: newUser?.id });
   } catch (error) {
-    console.log(error);
-    res.status(400).json({ error: "Email already exists" });
+    console.error(error);
+    res
+      .status(400)
+      .json({ error: "Could not create user. Email might be taken." });
   }
 });
-
 export default router;
