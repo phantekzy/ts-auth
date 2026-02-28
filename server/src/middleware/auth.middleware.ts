b229d6c6e80710d5ef422b337b93dc5e626def7a
phantekzy <mainilotfi@gmail.com>
2026-02-28 18:25:33 +0100

    cookie check

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -4,4 +4,9 @@ export const authenticate = async (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {};
+) => {
+  const sessionId = req.cookies.auth_session;
+  if (!sessionId) {
+    return res.status(401).json({ error: "Authentication required" });
+  }
+};
