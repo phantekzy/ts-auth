e5ee78d3e535d42af59d5a54206a82fd2413b425
phantekzy <mainilotfi@gmail.com>
2026-03-01 21:23:44 +0100

    middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,6 +1,4 @@
 import type { NextFunction, Request, Response } from "express";
-import { db } from "../db/index.js";
-import { sessions, users } from "../db/schema.js";
 
 export const authenticate = async (
   req: Request,
@@ -10,16 +8,5 @@ export const authenticate = async (
   const sessionId = req.cookies.auth_session;
   if (!sessionId) {
     return res.status(401).json({ error: "Authentication required" });
-  }
-  try {
-    const [sessionWithUser] = await db.select({
-      user: {
-        id: users.id,
-        email: users.email,
-      },
-      expiresAt: sessions.expiresAt,
-    });
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
   }
 };
