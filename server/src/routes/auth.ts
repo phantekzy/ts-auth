eacac6104f925ebf2d54fd6f5d44d372803915bb
phantekzy <mainilotfi@gmail.com>
2026-03-02 18:34:30 +0100

    Protected Route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { login, logout, signup } from "../controller/auth.controller.js";
+import { authenticate } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
+router.get("/me", authenticate, (req, res) => {
+  res.json({ user: (req as any).user });
+});
 export default router;
