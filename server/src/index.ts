c6df7218ca49d19eea802432bb87a4cbb269165c
phantekzy <mainilotfi@gmail.com>
2026-03-03 14:59:56 +0100

    push test

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(
 );
 
 app.use("/auth", authRoutes);
-
+//PORT SECTION
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
